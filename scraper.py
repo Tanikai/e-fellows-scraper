02c25bda6421e35052337f359fc428aae7861a4d
Kai Anter <kaianter29@gmail.com>
2020-07-05 08:31:06 +0200

    Added: reading credentials

--- scraper.py
+++ scraper.py
@@ -12,4 +12,36 @@ class Scraper:
     def printLink(self):
         print('Link: ' + self.db_link)
 
+class CredentialReader:
+    email = ''
+    password = ''
+
+    def __init__(self):
+        import json
+        import os.path
+
+        jsoncreds = {}
+        if not os.path.isfile('credentials.json'):
+            jsoncreds['email'] = ''
+            jsoncreds['password'] = ''
+            try:
+                with open('credentials.json', 'w') as f:
+                    json.dump(jsoncreds, f, indent = 2)
+                    f.close
+            finally:
+                raise ValueError('File credentials.json doesn\'t exist! Please enter your credentials.')
+        else:
+            with open('credentials.json', 'r') as f:
+                jsoncreds = json.load(f)
+                f.close()            
+            if (jsoncreds['email'] == '') or (jsoncreds['password'] == ''):
+                raise ValueError('No credentials in credentials.json provided!')
+            else:
+                self.email = jsoncreds['email']
+                self.password = jsoncreds['password'] 
+
 x = Scraper('ct')
+
+y = CredentialReader()
+print('Email: ' + y.email)
+print('Password: ' + y.password)
