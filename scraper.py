4c71465f24709b12ac8bc2b8cbf19fd9448989a1
Kai Anter <kaianter29@gmail.com>
2020-07-05 10:14:20 +0200

    Added: Login + Login-Check

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import os.path
+import time
 
 class Scraper:
     FDriver = ''
@@ -19,6 +20,8 @@ class Scraper:
         else:
             self.FDownloadPath = ADownloadpath
 
+        self.createWebdriver()
+
     def createWebdriver(self):
         # Profile settings
         ffProfile = webdriver.FirefoxProfile()
@@ -33,7 +36,29 @@ class Scraper:
     
     def eFellowsLogin(self, AEmail, APassword):
         self.FDriver.get('https://www.e-fellows.net/')
+        self.FDriver.find_element_by_class_name("btn-login").click()
+        LInputEmail = self.FDriver.find_element_by_name("Login")
+        LInputPassword = self.FDriver.find_element_by_name("Password")
+        LInputEmail.send_keys(AEmail)
+        LInputPassword.send_keys(APassword)
+        LInputEmail.submit()
+
+        time.sleep(7) # Todo: Wait until submit is finished
         
+        LErrorResponses = []
+        try:
+            # No login-response element when login was successful
+            LResponseParent = self.FDriver.find_element_by_class_name('login-response')
+            LErrorResponses = LResponseParent.find_elements_by_tag_name('p')
+        except:
+            pass
+        # Check for login error codes
+        for LResponse in LErrorResponses:
+            if LResponse.text != '':
+                raise Exception('Login error: ' + LResponse.text)
+
+    def stopScraper(self):
+        self.FDriver.quit()
 
 class CredentialReader:
     FEmail = ''
@@ -62,4 +87,8 @@ class CredentialReader:
                 self.FEmail = jsoncreds['email']
                 self.FPassword = jsoncreds['password'] 
 
+FCreds = CredentialReader()
+
 x = Scraper('ct', '')
+x.eFellowsLogin(FCreds.FEmail, FCreds.FPassword)
+x.stopScraper()
